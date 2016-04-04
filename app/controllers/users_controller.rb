ecba926d4767aee7d2b2bb1c8d830cb6f172aae9
Alexandr Orlov <morozhunce@gmail.com>
2016-04-04 14:40:35 +0300

    zdsgsdg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @friends
+    @friends = get_friends
   end
 
   def show
@@ -48,5 +48,33 @@ class UsersController < ApplicationController
     }
 
     VkRequest.link(request_params)
+  end
+
+  def get_friends
+    params = VkRequest.form_params({
+                                       :user_id => cookies[:uid],
+                                       :order => 'hints',
+                                       :access_token => '00aa695bb656facb72a2215604fc65a2d3dd3131491de154ca05b5d34edd98c1bd36c2dfc8e5e05f5cc69',
+                                       :v => '5.37'
+                                   })
+
+    request_params = {
+        :url => 'api.vk.com',
+        :method => 'friends.get',
+        :params => params
+    }
+
+    response = VkRequest.perform(request_params, true)
+    friends = response.map{|key, value| value['items'] }.first
+    params = VkRequest.form_params({ :user_ids => friends.map(&:to_s).join('%2C%20') })
+
+    request_params = {
+        :url => 'api.vk.com',
+        :method => 'users.get',
+        :params => params
+    }
+
+    response = VkRequest.perform(request_params, true)
+    response['response'].map{ |friend| { first_name: friend['first_name'], last_name: friend['last_name'] } }
   end
 end
